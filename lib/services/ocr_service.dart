cbdbba66903c1bdff0b7d1e254c314c67c3a1dd7
mubinsyedmuhammed8 <mubinsyedmuhammed8@gmail.com>
2025-01-25 18:52:38 +0530

    New updates

--- lib/services/ocr_service.dart
+++ lib/services/ocr_service.dart
@@ -1,22 +1,24 @@
-// services/ocr_service.dart
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 
 class OCRService {
-  final String apiUrl = "http://localhost:8000/extract_text/";
+  final String apiUrl = "http://127.0.0.1:8000/extract_text/";  // Fixed endpoint
 
   Future<String> extractText(Uint8List imageBytes) async {
-    try {
-      var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
-      request.files.add(
-        http.MultipartFile.fromBytes('file', imageBytes, filename: 'image.jpg'),
-      );
+    var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
 
+    try {
+      // Add the image file to the request
+      request.files.add(http.MultipartFile.fromBytes('file', imageBytes, filename: 'image.jpg'));
+
+      // Send the request and await the response
       var response = await request.send();
+
       if (response.statusCode == 200) {
         final responseData = await response.stream.bytesToString();
-        return json.decode(responseData)['extracted_text'] ?? '';
+        final extractedText = json.decode(responseData)['extracted_cleaned_text'];
+        return extractedText;
       } else {
         return 'Failed to extract text';
       }
