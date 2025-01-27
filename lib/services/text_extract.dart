3c50919705e1353e7f73df8e67d5e07562f3ee18
mubinsyedmuhammed8 <mubinsyedmuhammed8@gmail.com>
2025-01-27 11:58:36 +0530

    new commit before demo

--- lib/services/text_extract.dart
+++ lib/services/text_extract.dart
@@ -1,36 +1,36 @@
-// widgets/text_field_with_extraction.dart
-import 'package:flutter/material.dart';
+// // widgets/text_field_with_extraction.dart
+// import 'package:flutter/material.dart';
 
-class TextFieldWithExtraction extends StatelessWidget {
-  final String label;
-  final String field;
-  final Function(String) onExtract;
+// class TextFieldWithExtraction extends StatelessWidget {
+//   final String label;
+//   final String field;
+//   final Function(String) onExtract;
 
-  const TextFieldWithExtraction({
-    super.key,
-    required this.label,
-    required this.field,
-    required this.onExtract,
-  });
+//   const TextFieldWithExtraction({
+//     super.key,
+//     required this.label,
+//     required this.field,
+//     required this.onExtract,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    final controller = TextEditingController();
+//   @override
+//   Widget build(BuildContext context) {
+//     final controller = TextEditingController();
 
-    return Row(
-      children: [
-        Expanded(
-          child: TextFormField(
-            controller: controller,
-            decoration: InputDecoration(labelText: label),
-            validator: (value) => value!.isEmpty ? "Please enter your $label" : null,
-          ),
-        ),
-        IconButton(
-          icon: const Icon(Icons.text_fields, color: Colors.teal),
-          onPressed: () => onExtract(controller.text),
-        ),
-      ],
-    );
-  }
-}
+//     return Row(
+//       children: [
+//         Expanded(
+//           child: TextFormField(
+//             controller: controller,
+//             decoration: InputDecoration(labelText: label),
+//             validator: (value) => value!.isEmpty ? "Please enter your $label" : null,
+//           ),
+//         ),
+//         IconButton(
+//           icon: const Icon(Icons.text_fields, color: Colors.teal),
+//           onPressed: () => onExtract(controller.text),
+//         ),
+//       ],
+//     );
+//   }
+// }
