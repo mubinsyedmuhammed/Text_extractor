b50d81f951d95293f10486b764a230a0b93fb553
mubinsyedmuhammed8 <mubinsyedmuhammed8@gmail.com>
2025-01-28 13:22:12 +0530

    before edit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:text_extractor/Screen/home_screen.dart';
+import 'package:text_extractor/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
